0fda8fe58db167bee7155b047e708d2e96e0ffa3
endofthepain <ekaonin@gmail.com>
2024-08-08 22:48:08 +0700

    l

--- src/send_and_confirm.rs
+++ src/send_and_confirm.rs
@@ -27,8 +27,8 @@ const _SIMULATION_RETRIES: usize = 4;
 const GATEWAY_RETRIES: usize = 150;
 const CONFIRM_RETRIES: usize = 8;
 
-const CONFIRM_DELAY: u64 = 0;
-const GATEWAY_DELAY: u64 = 300; //300;
+const CONFIRM_DELAY: u64 = 500;
+const GATEWAY_DELAY: u64 = 0; //300;
 
 pub enum ComputeBudget {
     Dynamic,
